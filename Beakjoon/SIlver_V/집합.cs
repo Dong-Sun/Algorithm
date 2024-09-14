75f22472d99a7b4763420fc7c33f41fecd3eb4ef
Dong-Sun <duaehdjs20@gmail.com>
2024-09-14 23:49:39 +0900

    upload 2024-09-14
    c#으로 시간초과 문제를 해결할 땐 StringBuilder를 사용해 문자열을 붙여 한번에 출력(Console.WriteLine연산이 느리기에 이를 최소화 한다.)

---
+++ Beakjoon/SIlver_V/집합.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+
+namespace SilverV
+{
+    class _11723
+    {
+        static void Main(string[] args)
+        {
+            Solution();
+        }
+
+        public static void Solution()
+        {
+            int m = int.Parse(Console.ReadLine());
+            int num = 0;
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < m; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                string op = input[0];
+                if (op == "add")
+                {
+                    int x = int.Parse(input[1]);
+                    num |= (1 << x);
+                }
+                else if (op == "remove")
+                {
+                    int x = int.Parse(input[1]);
+                    num &= ~(1 << x);
+                }
+                else if (op == "check")
+                {
+                    int x = int.Parse(input[1]);
+                    if ((num & (1 << x)) == 0)
+                    {
+                        sb.Append(0 + "\n");
+                    }
+                    else
+                    {
+                        sb.Append(1 + "\n");
+                    }
+                }
+                else if (op == "toggle")
+                {
+                    int x = int.Parse(input[1]);
+                    num ^= (1 << x);
+                }
+                else if (op == "all")
+                {
+                    num = (1 << 21) - 1;
+                }
+                else if (op == "empty")
+                {
+                    num = 0;
+                }
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
