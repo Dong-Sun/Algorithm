a90821a64ae72b16d54e000cd1a1bc671956cbda
DongSun <duaehdtjs20@gmail.com>
2024-09-18 19:36:38 +0900

    upload 2024-09-18
    1. dfs와 bfs 구현하며 평소에 쓰던 로직에 예상치 못한 문제점 발견해냄(방문처리 시점 차이)
    2. 힙을 나름대로 구현하며 복습, 직접 구현이 많이 까다로웠다.(PriorityQueue 사용하면 몇 줄 안나오는 문제들)

---
+++ Beakjoon/SIlver_II/DFS와 BFS.cs
@@ -0,0 +1,81 @@
+﻿namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution();
+        }
+
+        public static void Solution()
+        {
+            string[] split = Console.ReadLine().Split();
+            int n = int.Parse(split[0]);
+            int m = int.Parse(split[1]);
+            int v = int.Parse(split[2]);
+            List<List<int>> Vertex = new List<List<int>>(n + 1);
+            for (int i = 0; i <= n; i++)
+                Vertex.Add(new List<int>(n + 1));
+
+            // 간선 연결
+            for (int i = 0; i < m; i++)
+            {
+                split = Console.ReadLine().Split();
+                int left = int.Parse(split[0]);
+                int right = int.Parse(split[1]);
+                Vertex[left].Add(right);
+                Vertex[right].Add(left);
+            }
+
+            //dfs
+            foreach (List<int> list in Vertex)
+            {
+                list.Sort();
+                list.Reverse();
+            }
+
+            bool[] visited = new bool[n + 1];
+            Stack<int> dfs = new Stack<int>();
+            dfs.Push(v);
+            while (dfs.Count > 0)
+            {
+                int cur = dfs.Pop();
+                if (visited[cur] == true)
+                    continue;
+                else
+                    visited[cur] = true;
+                Console.Write(cur + " ");
+                foreach (int next in Vertex[cur])
+                {
+                    if (visited[next] == false)
+                    {
+                        dfs.Push(next);
+                    }
+                }
+            }
+            Console.WriteLine();
+            // bfs
+            foreach (List<int> list in Vertex)
+                list.Reverse();
+            visited = new bool[n + 1];
+            Queue<int> bfs = new Queue<int>();
+            bfs.Enqueue(v);
+            while (bfs.Count > 0)
+            {
+                int cur = bfs.Dequeue();
+                if (visited[cur] == true)
+                    continue;
+                else
+                    visited[cur] = true;
+                Console.Write(cur + " ");
+                foreach (int next in Vertex[cur])
+                {
+                    if (visited[next] == false)
+                    {
+                        bfs.Enqueue(next);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Beakjoon/SIlver_II/최소 힙.cs
@@ -0,0 +1,69 @@
+﻿using System.Text;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution();
+        }
+
+        public static void Solution()
+        {
+            StringBuilder sb = new StringBuilder();
+            int n = int.Parse(Console.ReadLine());
+            int[] heap = new int[100005];
+            int index = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                if (input != 0)
+                {
+                    heap[++index] = input;
+                    int curIndex = index;
+                    while (heap[curIndex] < heap[curIndex / 2])
+                    {
+                        int temp = heap[curIndex];
+                        heap[curIndex] = heap[curIndex / 2];
+                        heap[curIndex / 2] = temp;
+                        curIndex = curIndex / 2;
+                        if (curIndex == 1)
+                            break;
+                    }
+                }
+                else
+                {
+                    if (index == 0)
+                    {
+                        sb.Append("0\n");
+                        continue;
+                    }
+                    sb.Append(heap[1] + "\n");
+                    heap[1] = heap[index--];
+                    int curIndex = 1;
+                    while (curIndex * 2 <= index)
+                    {
+                        int left = int.MaxValue;
+                        int right = int.MaxValue;
+                        if (curIndex * 2 <= index)
+                            left = heap[curIndex * 2];
+                        if (curIndex * 2 + 1 <= index)
+                            right = heap[curIndex * 2 + 1];
+                        int min = left < right ? curIndex * 2 : curIndex * 2 + 1;
+                        if (heap[curIndex] > heap[min])
+                        {
+                            int temp = heap[curIndex];
+                            heap[curIndex] = heap[min];
+                            heap[min] = temp;
+                            curIndex = min;
+                        }
+                        else
+                            break;
+                    }
+                }
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
