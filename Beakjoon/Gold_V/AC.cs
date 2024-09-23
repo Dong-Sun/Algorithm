a415e576c2d79229390c630129ff12b56ee64b1a
DongSun <duaehdtjs20@gmail.com>
2024-09-23 23:46:15 +0900

    upload 2024-09-23
    1. AC문제를 풀면서 다소 난해한 입력이 주어졌을 때 어떻게 정보를 빼올지에 대해 고민해 볼 수 있었다.
    2. bfs를 3차원 배열로 푸는게 생각보다 복잡했다.

---
+++ Beakjoon/Gold_V/AC.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+
+namespace Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            StringBuilder sb = new StringBuilder();
+            while (t > 0)
+            {
+                string p = Console.ReadLine();
+                int n = int.Parse(Console.ReadLine());
+                string arr = Console.ReadLine();
+                arr = arr.Substring(1, arr.Length - 2);
+                var list = n == 0 ? new List<int>() : Array.ConvertAll(arr.Split(','), int.Parse).ToList();
+                bool isError = false;
+                bool isReverse = false;
+
+                // 명령시작
+                for (int i = 0; i < p.Length; i++)
+                {
+                    // 뒤집기
+                    if (p[i].Equals('R'))
+                        isReverse = !isReverse;
+                    // 첫 번째 수 버리기
+                    else if (p[i].Equals('D'))
+                    {
+                        if (list.Count > 0)
+                        {
+                            if (!isReverse)
+                                list.RemoveAt(0);
+                            else
+                                list.RemoveAt(list.Count - 1);
+                        }
+                        else
+                            isError = true;
+                    }
+                }
+                if (isError)
+                    sb.Append("error\n");
+                else
+                {
+                    sb.Append("[");
+                    if (!isReverse)
+                    {
+                        for (int i = 0; i < list.Count; i++)
+                        {
+                            if (i == list.Count - 1)
+                                sb.Append($"{list[i]}");
+                            else
+                                sb.Append($"{list[i]},");
+                        }
+                    }
+                    else
+                    {
+                        for (int i = list.Count - 1; i >= 0; i--)
+                        {
+                            if (i == 0)
+                                sb.Append($"{list[i]}");
+                            else
+                                sb.Append($"{list[i]},");
+                        }
+                    }
+                    sb.Append("]\n");
+                }
+                t--;
+            }
+            Console.Write(sb.ToString());
+        }
+    }
+}
---
+++ Beakjoon/Gold_V/토마토.cs
@@ -0,0 +1,77 @@
+﻿using System.Data.SqlTypes;
+using System.Text;
+
+namespace Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] split = Console.ReadLine().Split();
+            int m = int.Parse(split[0]); // 가로
+            int n = int.Parse(split[1]); // 세로
+            int h = int.Parse(split[2]); // 높이
+            int result = 0;
+            int zero = 0;
+            int[,,] tomato = new int[h + 2, n + 2, m + 2];
+            bool[,,] visited = new bool[h + 2, n + 2, m + 2];
+            Queue<(int, int, int, int)> q = new Queue<(int, int, int, int)>();
+            for (int i = 1; i <= h; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    split = Console.ReadLine().Split(' ');
+                    for (int k = 1; k <= m; k++)
+                    {
+                        tomato[i, j, k] = int.Parse(split[k - 1]);
+                        if (tomato[i, j, k] == 1)
+                            q.Enqueue((i, j, k, 0));
+                        else if (tomato[i, j, k] == 0)
+                            zero++;
+                    }
+                }
+            }
+            if (zero == 0)
+            {
+                Console.WriteLine("0");
+                return;
+            }
+            while (q.Count > 0)
+            {
+                (int, int, int, int) cur = q.Dequeue();
+                int height = cur.Item1;
+                int col = cur.Item2;
+                int row = cur.Item3;
+                int distance = cur.Item4;
+                if (height > h || height <= 0)
+                    continue;
+                if (col > n || col <= 0)
+                    continue;
+                if (row > m || row <= 0)
+                    continue;
+                if (tomato[height, col, row] == -1)
+                    continue;
+                if (visited[height, col, row])
+                    continue;
+                visited[height, col, row] = true;
+                if (tomato[height, col, row] == 0)
+                {
+                    tomato[height, col, row] = distance;
+                    if (result < distance)
+                        result = distance;
+                    zero--;
+                }
+                q.Enqueue((height + 1, col, row, distance + 1));
+                q.Enqueue((height - 1, col, row, distance + 1));
+                q.Enqueue((height, col + 1, row, distance + 1));
+                q.Enqueue((height, col - 1, row, distance + 1));
+                q.Enqueue((height, col, row + 1, distance + 1));
+                q.Enqueue((height, col, row - 1, distance + 1));
+            }
+            if (zero == 0)
+                Console.WriteLine(result);
+            else
+                Console.WriteLine("-1");
+        }
+    }
+}
