19110db79512989bdc0ad5b4f8c519aec0379399
DongSun <duaehdtjs20@gmail.com>
2024-10-02 19:04:26 +0900

    upload 2024-10-02
    1. DSLR문제에서 메모리 초과 오류가 계속 뜸 -> bfs로직에 방문처리를 먼저 실행해서 큐에 넣는 자료를 최소한으로 줄였다. 앞으로의 bfs풀이 로직을 위 방식으로 수정해야 할듯하다.
    2. 테트로미노문제는 접근하는 방식이 까다로웠다. 혼자서는 모든 모양의 합을 구해서 최대값을 뽑아내는 방법밖에 안떠올라 인터넷을 보니 dfs를 통해 모든 모양을 탐색하는 아이디어를 얻었다. 하지만 이 방법으로 처리가 불가능한 블럭이 있었고 이는 따로 탐색해서 해결했다.

---
+++ Beakjoon/Gold_IV/DSLR.cs
@@ -0,0 +1,75 @@
+﻿namespace Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t > 0)
+            {
+                t--;
+                string[] split = Console.ReadLine().Split();
+                short start = short.Parse(split[0]);
+                short end = short.Parse(split[1]);
+                Queue<(short, string)> q = new Queue<(short, string)>();
+                bool[] visited = new bool[10000];
+                q.Enqueue((start, ""));
+                while (q.Count > 0)
+                {
+                    (short, string) temp = q.Dequeue();
+                    short cur = temp.Item1;
+                    string result = temp.Item2;
+                    if (cur == end)
+                    {
+                        Console.WriteLine(result);
+                        break;
+                    }
+                    (short, string) next = Left(cur, result);
+                    if (!visited[next.Item1])
+                    {
+                        visited[next.Item1] = true;
+                        q.Enqueue(next);
+                    }
+                    next = Right(cur, result);
+                    if (!visited[next.Item1])
+                    {
+                        visited[next.Item1] = true;
+                        q.Enqueue(next);
+                    }
+                    next = Double(cur, result);
+                    if (!visited[next.Item1])
+                    {
+                        visited[next.Item1] = true;
+                        q.Enqueue(next);
+                    }
+                    next = Subtract(cur, result);
+                    if (!visited[next.Item1])
+                    {
+                        visited[next.Item1] = true;
+                        q.Enqueue(next);
+                    }
+                }
+            }
+        }
+        static (short, string) Left(short n, string str)
+        {
+            short temp = (short)(n % 1000 * 10 + n / 1000);
+            return (temp, str + 'L');
+        }
+        static (short, string) Right(short n, string str)
+        {
+            short temp = (short)(n / 10 + n % 10 * 1000);
+            return (temp, str + 'R');
+        }
+        static (short, string) Double(short n, string str)
+        {
+            short temp = (short)(n * 2 % 10000);
+            return (temp, str + 'D');
+        }
+        static (short, string) Subtract(short n, string str)
+        {
+            short temp = (short)(n != 0 ? n - 1 : 9999);
+            return (temp, str + 'S');
+        }
+    }
+}
---
+++ Beakjoon/Gold_IV/테트로미노.cs
@@ -0,0 +1,69 @@
+﻿namespace Solution
+{
+    class Program
+    {
+        static int n = 0;
+        static int m = 0;
+        static int[,] table;
+        static int result = 0;
+        static void Main(string[] args)
+        {
+            // input
+            string[] split = Console.ReadLine().Split();
+            n = int.Parse(split[0]);
+            m = int.Parse(split[1]);
+            table = new int[n + 2, m + 2];
+            for (int i = 1; i <= n; i++)
+            {
+                split = Console.ReadLine().Split();
+                for (int j = 1; j <= m; j++)
+                {
+                    table[i, j] = int.Parse(split[j - 1]);
+                }
+            }
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= m; j++)
+                {
+                    Dfs(i, j, i, j, 1, 0);
+                }
+            }
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= m; j++)
+                {
+                    int sum = table[i, j] + table[i + 1, j] + table[i - 1, j] + table[i, j + 1] + table[i, j - 1];
+                    result = result >= sum - table[i + 1, j] ? result : sum - table[i + 1, j];
+                    result = result >= sum - table[i - 1, j] ? result : sum - table[i - 1, j];
+                    result = result >= sum - table[i, j + 1] ? result : sum - table[i, j + 1];
+                    result = result >= sum - table[i, j - 1] ? result : sum - table[i, j - 1];
+                }
+            }
+            Console.WriteLine(result);
+        }
+        static void Dfs(int y, int x, int prevY, int prevX, int depth, int sum)
+        {
+            if (y <= 0 || y > n)
+            {
+                return;
+            }
+            if (x <= 0 || x > m)
+            {
+                return;
+            }
+            if (depth == 4)
+            {
+                result = result >= sum + table[y, x] ? result : sum + table[y, x];
+                return;
+            }
+            if (!(y + 1 == prevY && x == prevX))
+                Dfs(y + 1, x, y, x, depth + 1, sum + table[y, x]);
+            if (!(y - 1 == prevY && x == prevX))
+                Dfs(y - 1, x, y, x, depth + 1, sum + table[y, x]);
+            if (!(y == prevY && x + 1 == prevX))
+                Dfs(y, x + 1, y, x, depth + 1, sum + table[y, x]);
+            if (!(y == prevY && x - 1 == prevX))
+                Dfs(y, x - 1, y, x, depth + 1, sum + table[y, x]);
+        }
+    }
+}
