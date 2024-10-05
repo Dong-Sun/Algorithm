9d56975c84b19f7b34f263df3b44cbc38f14d6e3
DongSun <duaehdtjs20@gmail.com>
2024-10-05 14:32:35 +0900

    upload 2024-10-05
    1. 오랜만에 보는 다익스트라 문제라 다시 공부했다.

---
+++ Beakjoon/Gold_V/최소비용 구하기.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+
+namespace Solution
+{
+    class Program
+    {
+        static StringBuilder sb = new StringBuilder();
+        static Func<int[]> ConvertToIntArray = () =>
+        {
+            return Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        };
+        static void Main(string[] args)
+        {
+            int[] input;
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            var node = new List<List<(int, int)>>();
+            for (int i = 0; i <= n; i++)
+                node.Add(new List<(int, int)>());
+
+            int[] distance = new int[n + 1];
+            for (int i = 0; i <= n; i++)
+                distance[i] = -1;
+
+            int temp = m;
+            while (temp > 0)
+            {
+                input = ConvertToIntArray();
+                int start = input[0];
+                int end = input[1];
+                int dist = input[2];
+                node[start].Add((end, dist));
+                temp--;
+            }
+
+            input = ConvertToIntArray();
+            int s = input[0];
+            int e = input[1];
+            distance[s] = 0;
+            PriorityQueue<(int, int), int> pq = new PriorityQueue<(int, int), int>();
+            pq.Enqueue((s, 0), 0);
+            while (pq.Count > 0)
+            {
+                (int, int) cur = pq.Dequeue();
+                int index = cur.Item1;
+                int dist = cur.Item2;
+                if (distance[index] < dist)
+                    continue;
+                foreach (var v in node[index])
+                {
+                    int next = v.Item1;
+                    int nextDist = v.Item2 + dist;
+                    if (distance[next] == -1 || nextDist < distance[next])
+                    {
+                        distance[next] = nextDist;
+                        pq.Enqueue((next, nextDist), -nextDist);
+
+                    }
+                }
+            }
+            Console.WriteLine(distance[e]);
+        }
+    }
+}
---
+++ Beakjoon/SIlver_I/구간 합 구하기 5.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+
+namespace Solution
+{
+    class Program
+    {
+        static StringBuilder sb = new StringBuilder();
+        static Func<int[]> ConvertToInt = () =>
+        {
+            return Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        };
+        static void Main(string[] args)
+        {
+            int[] input = ConvertToInt.Invoke();
+            int n = input[0];
+            int m = input[1];
+            int[,] coordinate = new int[m, 4];
+            int[,] table = new int[n + 2, n + 2];
+
+            for (int i = 1; i <= n; i++)
+            {
+                input = ConvertToInt.Invoke();
+                for (int j = 0; j < n; j++)
+                {
+                    table[i, j + 1] = input[j];
+                }
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                input = ConvertToInt.Invoke();
+                for (int j = 0; j < 4; j++)
+                {
+                    coordinate[i, j] = input[j];
+                }
+            }
+
+            int[,] dp = new int[n + 2, n + 2];
+            for (int y = 1; y <= n; y++)
+            {
+                for (int x = 1; x <= n; x++)
+                {
+                    dp[y, x] = dp[y - 1, x] + dp[y, x - 1] - dp[y - 1, x - 1] + table[y, x];
+                }
+            }
+            for (int i = 0; i < m; i++)
+            {
+                int y1 = coordinate[i, 0];
+                int x1 = coordinate[i, 1];
+                int y2 = coordinate[i, 2];
+                int x2 = coordinate[i, 3];
+
+                sb.AppendLine((dp[y2, x2] - dp[y1 - 1, x2] - dp[y2, x1 - 1] + dp[y1 - 1, x1 - 1]).ToString());
+            }
+            Console.Write(sb);
+        }
+    }
+}
