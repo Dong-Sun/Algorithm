1710bf69fc0ff3ab5b1750d61c71f6e1d4af25fa
DongSun <duaehdtjs20@gmail.com>
2024-10-07 22:55:44 +0900

    2024-10-07
    1. LCS(Longest Common Subsequence) 최장 공통 부분수열 알고리즘을 배웠다.
    2. 숨바꼭질 문제를 풀면서 다익스트라에 거리를 측정하기 위한 배열을 초기화 할때는 항상 최대값 설정에 주의해야 한다.

---
+++ Beakjoon/Gold_V/LCS.cs
@@ -0,0 +1,23 @@
+﻿namespace Debug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = Console.ReadLine();
+            string second = Console.ReadLine();
+            int[,] dp = new int[second.Length + 1, first.Length + 1];
+            for (int i = 1; i <= second.Length; i++)
+            {
+                for (int j = 1; j <= first.Length; j++)
+                {
+                    if (second[i - 1].Equals(first[j - 1]))
+                        dp[i, j] = dp[i - 1, j - 1] + 1;
+                    else
+                        dp[i, j] = Math.Max(dp[i - 1, j], dp[i, j - 1]);
+                }
+            }
+            Console.WriteLine(dp[second.Length, first.Length]);
+        }
+    }
+}
---
+++ Beakjoon/Gold_V/숨바꼭질 3.cs
@@ -0,0 +1,47 @@
+﻿namespace Debug
+{
+    class Program
+    {
+        static Func<int[]> InputIntArray = () => Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        static void Main(string[] args)
+        {
+            int[] input = InputIntArray();
+            int n = input[0];
+            int k = input[1];
+            const int MAX = 100001;
+            int[] distance = Enumerable.Repeat(100005, 100001).ToArray();
+            bool[] visited = new bool[MAX];
+            PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
+            pq.Enqueue(n, 0);
+            distance[n] = 0;
+            while (pq.Count > 0)
+            {
+                int cur = pq.Dequeue();
+                int second = distance[cur];
+                if (cur == k)
+                {
+                    Console.WriteLine(second);
+                    break;
+                }
+                if (visited[cur])
+                    continue;
+                visited[cur] = true;
+                if (cur * 2 < MAX && second < distance[cur * 2])
+                {
+                    distance[cur * 2] = second;
+                    pq.Enqueue(cur * 2, second);
+                }
+                if (cur + 1 < MAX && second + 1 < distance[cur + 1])
+                {
+                    distance[cur + 1] = second + 1;
+                    pq.Enqueue(cur + 1, second + 1);
+                }
+                if (cur - 1 >= 0 && second + 1 < distance[cur - 1])
+                {
+                    distance[cur - 1] = second + 1;
+                    pq.Enqueue(cur - 1, second + 1);
+                }
+            }
+        }
+    }
+}
