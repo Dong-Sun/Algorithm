f9e394566af052ad44ffca0bb769c6599327c946
DongSun <duaehdtjs20@gmail.com>
2024-10-09 01:55:38 +0900

    upload 2024-10-08

---
+++ Beakjoon/Gold_V/치킨 배달.cs
@@ -0,0 +1,79 @@
+﻿namespace Debug
+{
+    class Program
+    {
+        static int n;
+        static int m;
+        static int result = int.MaxValue;
+        static int[,] distance;
+        static int[] select;
+        static bool[] visited;
+        static List<(int, int)> home;
+        static List<(int, int)> chicken;
+        static Func<int[]> InputIntArray = () => Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        static void Main(string[] args)
+        {
+            int[] arr = InputIntArray.Invoke();
+            n = arr[0];
+            m = arr[1];
+            home = new List<(int, int)>();
+            chicken = new List<(int, int)>();
+
+            for (int i = 0; i < n; i++)
+            {
+                arr = InputIntArray.Invoke();
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (arr[j] == 1)
+                        home.Add((i, j));
+                    if (arr[j] == 2)
+                        chicken.Add((i, j));
+                }
+            }
+            select = new int[m];
+            visited = new bool[chicken.Count];
+            distance = new int[home.Count, chicken.Count];
+            for (int i = 0; i < home.Count; i++)
+            {
+                for (int j = 0; j < chicken.Count; j++)
+                {
+                    distance[i, j] = Math.Abs(home[i].Item1 - chicken[j].Item1) + Math.Abs(home[i].Item2 - chicken[j].Item2);
+                }
+            }
+            for (int i = 0; i < chicken.Count; i++)
+                Dfs(0, i);
+
+            Console.WriteLine(result);
+        }
+
+        static void Dfs(int depth, int cur)
+        {
+            select[depth] = cur;
+            if (depth == m - 1)
+            {
+                int sum = 0;
+                for (int i = 0; i < home.Count; i++)
+                {
+                    int min = int.MaxValue;
+                    foreach (var v in select)
+                    {
+                        min = Math.Min(min, distance[i, v]);
+                    }
+                    sum += min;
+                }
+                if (sum < result)
+                    result = sum;
+                return;
+            }
+            for (int i = cur + 1; i < chicken.Count; i++)
+            {
+                if (!visited[i])
+                {
+                    visited[i] = true;
+                    Dfs(depth + 1, i);
+                    visited[i] = false;
+                }
+            }
+        }
+    }
+}
---
+++ Beakjoon/Gold_V/파이프 옮기기 1.cs
@@ -0,0 +1,44 @@
+﻿namespace Debug
+{
+    class Program
+    {
+        static Func<int[]> InputIntArray = () => Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        static Func<int> InputInt = () => int.Parse(Console.ReadLine());
+        static void Main(string[] args)
+        {
+            int n = InputInt.Invoke();
+            int[,] table = new int[n + 2, n + 2];
+            for (int i = 1; i <= n; i++)
+            {
+                int[] temp = InputIntArray.Invoke();
+                for (int j = 1; j <= n; j++)
+                    table[i, j] = temp[j - 1];
+            }
+            int[,] dp = new int[n + 2, n + 2];
+            // dir 0 = 오른쪽, 1 = 대각, 2 = 아레
+            //      y    x      dir
+            Queue<((int, int), int)> bfs = new Queue<((int, int), int)>();
+            bfs.Enqueue(((1, 2), 0));
+            while (bfs.Count > 0)
+            {
+                ((int, int), int) cur = bfs.Dequeue();
+                (int, int) pos = cur.Item1;
+                int dir = cur.Item2;
+                int y = pos.Item1;
+                int x = pos.Item2;
+                if (y > n || y <= 0)
+                    continue;
+                if (x > n || x <= 0)
+                    continue;
+                dp[y, x]++;
+                if (table[y, x + 1] != 1 && table[y + 1, x] != 1 && table[y + 1, x + 1] != 1)
+                    bfs.Enqueue(((y + 1, x + 1), 1));
+                if (dir != 0 && table[y + 1, x] != 1)
+                    bfs.Enqueue(((y + 1, x), 2));
+                if (dir != 2 && table[y, x + 1] != 1)
+                    bfs.Enqueue(((y, x + 1), 0));
+            }
+            Console.WriteLine(dp[n, n]);
+        }
+    }
+}
